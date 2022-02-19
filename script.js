16ea29724db9d8f53dc0d178e7f3f64f60cdfc53
tomvieir <wevertonbvieira@gmail.com>
2022-02-19 18:08:02 -0300

    :up: simplificação da function para reutilizações

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const hamburguer = document.querySelector(".hamburguer")
 
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".sidebar").classList.toggle("show-menu")
+
+document.querySelector(".hamburguer").addEventListener("click", () =>{
+    document.querySelector(".container").classList.toggle("show-menu")
 })
