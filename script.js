4c7ce2307bd741dce22b769ccfac6f8a0d30005a
tomvieir <wevertonbvieira@gmail.com>
2022-02-22 22:43:46 -0300

    :package: Nova função para animação de scroll e otimização.

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 
-
+//animação hamburguer
 document.querySelector(".hamburguer").addEventListener("click", () =>{
     document.querySelector(".container").classList.toggle("show-menu")
 })
+
+
+//debounce
+function debounce(func, wait, immediate) {
+	let timeout
+	return function(...args) {
+		const context = this
+		const later = function() {
+			timeout = null
+			if (!immediate) func.apply(context, args)
+		}
+		const callNow = immediate && !timeout
+		clearTimeout(timeout)
+		timeout = setTimeout(later, wait)
+		if (callNow) func.apply(context, args)
+	}
+}
+
+
+
+// animação scrool =============================
+const target = document.querySelectorAll("[data-anime]")
+
+const animationClass = "animate"
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    target.forEach(function(element) {
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass)
+        } else {
+            element.classList.remove(animationClass)
+        }
+        
+    })
+}
+
+if(target.length) {
+    window.addEventListener('scroll', debounce(function() {
+        animeScroll()
+    },10))
+}
+
+
+// ==============================================
